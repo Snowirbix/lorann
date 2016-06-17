b8aef29d6969e4863dbd170a91ed27715ff4b445
Snowirbix <Snowirbix@DESKTOP-V15IQGI>
2016-06-17 12:41:16 +0200

    .

--- view/src/main/java/view/ViewPanel.java
+++ view/src/main/java/view/ViewPanel.java
@@ -73,34 +73,36 @@ class ViewPanel extends JPanel implements Observer {
 		graphics.clearRect(0, 0, this.getWidth(), this.getHeight());
 		MotionLessElement[][] map = this.getViewFrame().getModel().getMap();
 		ArrayList<IMobile> mobiles = this.getViewFrame().getModel().getMobiles();
-		for(int y = 0; y < map[0].length; y++) {
-			for(int x = 0; x < map.length; x++) {
-				graphics.drawImage(map[x][y].getImage(), x*32, y*32, 32, 32, this);
-			}
-		}
-		for(int i = mobiles.size()-1; i >= 0; i--) {
-			IMobile mobile = mobiles.get(i);
-			mobile.move();
-			if(map[mobile.getPosition().x][mobile.getPosition().y] instanceof ITouchable) {
-				((ITouchable) map[mobile.getPosition().x][mobile.getPosition().y]).onTouch(mobile);
-			}
-			for(int j = mobiles.size()-1; j >= 0; j--) {
-				IMobile mobile2 = mobiles.get(j);
-				if(mobile2 instanceof ITouchable && mobile2.getPosition().x == mobile.getPosition().x && mobile2.getPosition().y == mobile.getPosition().y) {
-					if(((ITouchable) mobile2).onTouch(mobile)) {
-						mobiles.remove(j);
-					}
+		if(map != null) {
+			for(int y = 0; y < map[0].length; y++) {
+				for(int x = 0; x < map.length; x++) {
+					graphics.drawImage(map[x][y].getImage(), x*32, y*32, 32, 32, this);
 				}
 			}
-			graphics.drawImage(mobile.getImage(), mobile.getPosition().x*32, mobile.getPosition().y*32, 32, 32, this);
+			for(int i = mobiles.size()-1; i >= 0; i--) {
+				IMobile mobile = mobiles.get(i);
+				mobile.move();
+				if(map[mobile.getPosition().x][mobile.getPosition().y] instanceof ITouchable) {
+					((ITouchable) map[mobile.getPosition().x][mobile.getPosition().y]).onTouch(mobile);
+				}
+				for(int j = mobiles.size()-1; j >= 0; j--) {
+					IMobile mobile2 = mobiles.get(j);
+					if(mobile2 instanceof ITouchable && mobile2.getPosition().x == mobile.getPosition().x && mobile2.getPosition().y == mobile.getPosition().y) {
+						if(((ITouchable) mobile2).onTouch(mobile)) {
+							mobiles.remove(j);
+						}
+					}
+				}
+				graphics.drawImage(mobile.getImage(), mobile.getPosition().x*32, mobile.getPosition().y*32, 32, 32, this);
+			}
+			graphics.setFont(new Font("Calibri", Font.PLAIN, 30));
+			graphics.drawString("Score : " + ((IHero) mobiles.get(0)).getScore(), map.length*32-30, map[0].length*32+30);
 		}
 		try {
 			Thread.sleep(150);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		graphics.setFont(new Font("Calibri", Font.PLAIN, 30));
-		graphics.drawString("Score : " + ((IHero) mobiles.get(0)).getScore(), map.length*32-30, map[0].length*32+30);
 		this.repaint();
 	}
 }
