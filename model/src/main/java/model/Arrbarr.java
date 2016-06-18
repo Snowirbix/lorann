1f4c5a655b3e987c1e93c6680cf7962c168cc1f7
Snowirbix <Snowirbix@192.168.1.74>
2016-06-18 20:20:17 +0200

    nerf this !

--- model/src/main/java/model/Arrbarr.java
+++ model/src/main/java/model/Arrbarr.java
@@ -13,52 +13,56 @@ public class Arrbarr implements IStrategy {
 
 	public Point move(Point positionDemon, IHero hero) {
 		
-		Point diff;
-		Point dir;
-		if(hero.getFireBall() != null) {
-			diff = new Point(hero.getFireBall().getPosition().x-positionDemon.x, hero.getFireBall().getPosition().y-positionDemon.y);
-			dir = hero.getFireBall().getDirection();
-		} else {
-			diff = new Point(0, 0);
-			dir = new Point(0, 0); // avoid the dodge
-		}
+		Point heroIsMoving = new Point(hero.getDirection().x, hero.getDirection().y);
 		
-		if(diff.x < 3 && diff.x > 0 && dir.x == -1) {
-			positionDemon.y++;
-		} else if(diff.x > -3 && diff.x < 0 && dir.x == 1) {
-			positionDemon.y--;
-		} else if(diff.y < 3 && diff.y > 0 && dir.y == -1) {
-			positionDemon.x++;
-		} else if(diff.y > -3 && diff.y < 0 && dir.y == 1) {
-			positionDemon.x--;
-		} else {
-			diff = new Point(hero.getPosition().x-positionDemon.x, hero.getPosition().y-positionDemon.y);
+		if(heroIsMoving.x != 0 || heroIsMoving.y != 0) {
 			
-			if(Math.abs(diff.x) > Math.abs(diff.y)) {
-				if(diff.x > 0) {
-					positionDemon.x++;
-				} else {
-					positionDemon.x--;
-				}
-				if(diff.y > 0) {
-					positionDemon.y++;
-				} else if(diff.y < 0) {
-					positionDemon.y--;
-				}
+			Point diff;
+			Point dir;
+			if(hero.getFireBall() != null) {
+				diff = new Point(hero.getFireBall().getPosition().x-positionDemon.x, hero.getFireBall().getPosition().y-positionDemon.y);
+				dir = hero.getFireBall().getDirection();
 			} else {
-				if(diff.y > 0) {
-					positionDemon.y++;
+				diff = new Point(0, 0);
+				dir = new Point(0, 0); // avoid the dodge
+			}
+			
+			if(diff.x < 3 && diff.x > 0 && dir.x == -1) {
+				positionDemon.y++;
+			} else if(diff.x > -3 && diff.x < 0 && dir.x == 1) {
+				positionDemon.y--;
+			} else if(diff.y < 3 && diff.y > 0 && dir.y == -1) {
+				positionDemon.x++;
+			} else if(diff.y > -3 && diff.y < 0 && dir.y == 1) {
+				positionDemon.x--;
+			} else {
+				diff = new Point(hero.getPosition().x-positionDemon.x, hero.getPosition().y-positionDemon.y);
+				
+				if(Math.abs(diff.x) > Math.abs(diff.y)) {
+					if(diff.x > 0) {
+						positionDemon.x++;
+					} else {
+						positionDemon.x--;
+					}
+					if(diff.y > 0) {
+						positionDemon.y++;
+					} else if(diff.y < 0) {
+						positionDemon.y--;
+					}
 				} else {
-					positionDemon.y--;
-				}
-				if(diff.x > 0) {
-					positionDemon.x++;
-				} else if(diff.x < 0) {
-					positionDemon.x--;
+					if(diff.y > 0) {
+						positionDemon.y++;
+					} else {
+						positionDemon.y--;
+					}
+					if(diff.x > 0) {
+						positionDemon.x++;
+					} else if(diff.x < 0) {
+						positionDemon.x--;
+					}
 				}
 			}
 		}
-		
 		return positionDemon;
 	}
 
